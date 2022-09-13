9ea8e29f5c6fa1dd6dc309f0ce7623843982cd31
Manu <mcabral@frba.utn.edu.ar>
2022-09-13 15:01:57 -0300

    feat: add movie controller

---
+++ src/controllers/movie.controller.js
@@ -0,0 +1,36 @@
+const { Movie } = require('../models')
+
+const getMovies = async (req, res) => {
+  try {
+    const movies = await Movie.findAll()
+    if (movies.length == 0) res.status(404).json({ message: 'No movies found' })
+    else res.json(movies)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+/**
+ * Creates a new movie in the database.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
+const createMovie = async (req, res) => {
+  const { title, image, releaseDate, qualification } = req.body
+  try {
+    const movie = await Movie.create({
+      title,
+      image,
+      releaseDate,
+      qualification,
+    })
+    res.status(201).json(movie)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+module.exports = {
+  getMovies,
+  createMovie,
+}
