215132f8adb39cb275ea42c72fd7f83a3775512a
Manu <mcabral@frba.utn.edu.ar>
2022-09-13 15:01:05 -0300

    refactor: change some names

--- src/database.js
+++ src/database.js
@@ -1,33 +1,32 @@
 /**
  * @file Contains the database configuration.
- * @author Manuel Cabral
  */
 
 const { Sequelize } = require('sequelize')
 const { DATABASE } = require('./config')
 
 const sequelize = new Sequelize(
-	DATABASE.NAME,
-	DATABASE.USERNAME,
-	DATABASE.PASSWORD,
-	{
-		host: DATABASE.HOST,
-		dialect: DATABASE.DIALECT,
-	}
+  DATABASE.NAME,
+  DATABASE.USERNAME,
+  DATABASE.PASSWORD,
+  {
+    host: DATABASE.HOST,
+    dialect: DATABASE.DIALECT,
+  }
 )
 
 const connectDatabase = async () => {
-	try {
-		await sequelize.sync({
-			force: false,
-		})
-		console.log(`Database ${DATABASE.NAME} connected successfully`)
-	} catch (error) {
-		console.log('Unable to connect to the database:', error)
-	}
+  try {
+    await sequelize.sync({
+      force: false,
+    })
+    console.log(`Database ${DATABASE.NAME} connected successfully`)
+  } catch (error) {
+    console.log('Unable to connect to the database:', error)
+  }
 }
 
 module.exports = {
-	sequelize,
-	connectDatabase,
+  sequelize,
+  connectDatabase,
 }
