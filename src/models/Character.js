968a62d13eebe0445de0dabab4574955b177ba51
Manu <mcabral@frba.utn.edu.ar>
2022-09-13 14:56:01 -0300

    feat: add character model

---
+++ src/models/Character.js
@@ -0,0 +1,32 @@
+/**
+ * @file Character model represents a character from disney
+ */
+
+const Sequelize = require('sequelize')
+const { sequelize } = require('../database')
+
+const Character = sequelize.define('character', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+  },
+  image: {
+    type: Sequelize.STRING,
+  },
+  age: {
+    type: Sequelize.INTEGER,
+  },
+  weight: {
+    type: Sequelize.FLOAT,
+  },
+  history: {
+    // limit to 64 KB (65.535 characters)
+    type: Sequelize.TEXT,
+  },
+})
+
+module.exports = Character
