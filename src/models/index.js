8c20c24e911f4d3c3cb98ba391197c1d6e38b528
Manu <mcabral@frba.utn.edu.ar>
2022-09-16 19:32:11 -0300

    feat: add genre relationship

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,5 @@
 /**
- * @file Contains the database models exports and associations.
+ * @file Contains the database models exports and relationships.
  */
 
 // require all models
@@ -7,13 +7,19 @@ const MovieCast = require('./MovieCast')
 const Character = require('./Character')
 const Movie = require('./Movie')
 const User = require('./User')
+const Genre = require('./Genre')
 
 // M-N relationship between movies and characters
 Movie.belongsToMany(Character, { through: MovieCast })
 Character.belongsToMany(Movie, { through: MovieCast })
 
+// 1-N relationship between movies and genres
+Movie.belongsTo(Genre)
+Genre.hasMany(Movie)
+
 module.exports = {
   Character,
   Movie,
   User,
+  Genre,
 }
