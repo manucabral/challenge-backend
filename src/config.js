29a220c8041f774fcb03c58c0ba8e24f8e61a0d2
Manu <mcabral@frba.utn.edu.ar>
2022-09-16 17:58:30 -0300

    chore: add email envs

--- src/config.js
+++ src/config.js
@@ -13,6 +13,11 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   PROTOCOL: process.env.PROTOCOL || 'http',
 
+  EMAIL: {
+    API_KEY: process.env.SENDGRID_API_KEY,
+    FROM: process.env.EMAIL_FROM,
+  },
+
   JWT: {
     SECRET: process.env.JWT_SECRET || 'secretwow',
     EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
