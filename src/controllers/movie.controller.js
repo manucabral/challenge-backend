35eacc17e2e00e1614c7335a47c78cf1f975007a
Manu <mcabral@frba.utn.edu.ar>
2022-09-16 19:31:09 -0300

    feat: add movie remaining routes

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -2,12 +2,24 @@
  * @file Contains movie controller
  */
 
-const { Movie } = require('../models')
+// require modules
+const { Op } = require('sequelize')
+const { Movie, Character, Genre } = require('../models')
 const { uploadImage } = require('../utils/files')
 
+/**
+ * Get all movies from the database.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
 const getMovies = async (req, res) => {
+  if (Object.keys(req.query).length > 0) return filterMovies(req, res)
   try {
-    const movies = await Movie.findAll()
+    const movies = await Movie.findAll({
+      include: {
+        model: Genre,
+      },
+    })
     if (movies.length == 0) res.status(404).json({ message: 'No movies found' })
     else res.json(movies)
   } catch (error) {
@@ -21,10 +33,12 @@ const getMovies = async (req, res) => {
  * @param {Object} res - The response object.
  */
 const createMovie = async (req, res) => {
-  const { title, releaseDate, qualification } = req.body
-  if (!title || !qualification)
+  const { title, releaseDate, qualification, genreId } = req.body
+  if (!title || !qualification || !genreId)
     return res.status(400).json({ message: 'Missing required fields' })
   try {
+    const genre = await Genre.findOne({ where: { id: genreId } })
+    if (!genre) return res.status(404).json({ message: 'Genre not found' })
     const image_url = await uploadImage(req.files)
     const movie = await Movie.create({
       title,
@@ -32,7 +46,91 @@ const createMovie = async (req, res) => {
       releaseDate,
       qualification,
     })
-    res.status(201).json(movie)
+    await genre.addMovie([movie])
+    res.status(201).json({ message: 'Movie created successfully', movie })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+/**
+ * Get one movie from the database.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
+const getOneMovie = async (req, res) => {
+  try {
+    const movie = await Movie.findOne({
+      where: { id: req.params.id },
+      include: {
+        model: Character,
+        attributes: ['image', 'name'],
+      },
+    })
+    if (!movie) res.status(404).json({ message: 'Movie not found' })
+    else res.json(movie)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+/**
+ * Update a movie in the database.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
+const updateMovie = async (req, res) => {
+  const { title, releaseDate, qualification, genreId } = req.body
+  if (!title || !qualification)
+    return res.status(400).json({ message: 'Missing required fields' })
+  try {
+    const genre = await Genre.findOne({ where: { id: genreId } })
+    if (!genre) return res.status(404).json({ message: 'Genre not found' })
+    const movie = await Movie.findOne({ where: { id: req.params.id } })
+    if (!movie) return res.status(404).json({ message: 'Movie not found' })
+    const image_url = req.files.image
+      ? await uploadImage(req.files)
+      : movie.image
+    await movie.update({ title, image: image_url, releaseDate, qualification })
+    await genre.addMovie([movie])
+    res.json({ message: 'Movie updated successfully', movie })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const deleteMovie = async (req, res) => {
+  try {
+    const movie = await Movie.findOne({ where: { id: req.params.id } })
+    if (!movie) return res.status(404).json({ message: 'Movie not found' })
+    await movie.destroy()
+    res.json({ message: 'Movie deleted successfully' })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const filterMovies = async (req, res) => {
+  const availableFilters = ['name', 'genre', 'order']
+  const filterFields = Object.keys(req.query)[0]
+  const value = req.query[filterFields]
+
+  if (!availableFilters.includes(filterFields))
+    return res.status(400).json({ message: 'Invalid filter' })
+
+  const options = {
+    where: {},
+    include: {
+      model: Genre,
+    },
+  }
+  if (filterFields === 'name') options.where.title = { [Op.like]: `%${value}%` }
+  if (filterFields === 'genre') options.where.genreId = value
+  if (filterFields === 'order') options.order = [['releaseDate', value]]
+  try {
+    const movies = await Movie.findAll(options)
+    if (movies.length == 0) res.status(404).json({ message: 'No movies found' })
+    else res.json(movies)
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
@@ -40,5 +138,8 @@ const createMovie = async (req, res) => {
 
 module.exports = {
   getMovies,
+  getOneMovie,
   createMovie,
+  updateMovie,
+  deleteMovie,
 }
--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,9 +1,27 @@
 const { Router } = require('express')
 const router = Router()
 
-const { getMovies, createMovie } = require('../controllers/movie.controller')
+const {
+  getMovies,
+  getOneMovie,
+  createMovie,
+  updateMovie,
+  deleteMovie,
+} = require('../controllers/movie.controller')
 
+// GET all movies
 router.get('/', getMovies)
+
+// GET one movie
+router.get('/:id', getOneMovie)
+
+// POST a new movie
 router.post('/', createMovie)
+
+// PUT update a movie (all fields)
+router.put('/:id', updateMovie)
+
+// DELETE a movie
+router.delete('/:id', deleteMovie)
 
 module.exports = router
